aebb651715fa70d0f2de4ddd70c1afe3c695eb1a
Johannes Lagler-Grüner <johannes.lagler-gruener@acp.at>
2021-02-04 15:38:03 +0100

    V2

---
+++ 1_Use Azure private Marketplace/deploy.ps1
@@ -0,0 +1,11 @@
+#region Assign Permission to User
+
+    $TenantID = "c3e2ecf2-3901-4a7c-84c1-738e9594da6d"
+
+    Connect-AzAccount -Tenant $TenantID
+    $marketplaceRole= "Marketplace Admin"
+    $UsernameToAssignRoleFor = "<UPN wher role should be assign>"
+    $MarketPlaceAdminRole = Get-AzRoleDefinition $marketplaceRole
+    New-AzRoleAssignment -SignInName $UsernameToAssignRoleFor -RoleDefinitionName $MarketPlaceAdminRole.Name -Scope "/providers/Microsoft.Marketplace"
+
+#endregion
